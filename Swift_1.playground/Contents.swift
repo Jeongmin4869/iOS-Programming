6b987259a1c64542bfacc893a26a4771e85182f1
jeongmin4869 <ljm4135@naver.com>
2022-08-31 21:40:41 +0900

    Swift문법

---
+++ Swift_1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// var : 변수선언
+// let : 상수선언
+
+var str = "Hello, playground"   // Hello, playground
+str = "Hello, Swift"            // Hello, Swift
+let constStr = str              // Hello, Swift
+constStr = "Hello, world"       // error
+
+// 타입지정
+// var variableName: Type
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+
+//숫자와 불린 타입
