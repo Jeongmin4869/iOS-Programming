d319dcd76ea73bcb988eeafbea864e032fb2cc5b
jeongmin4869 <ljm4135@naver.com>
2022-09-10 21:40:13 +0900

    iOS with Swift (텍스트 필드 변경처리)

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -6,9 +6,13 @@ class ConversionViewControlller : UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     
     @IBAction func fahrenheitEditingChanged(textField: UITextField){
-        celsiusLabel.text = textField.text
+        
+        if let text = textField.text, !text.isEmpty{
+            celsiusLabel.text = textField.text
+        }
+        else{
+            celsiusLabel.text = "???"
+        }
     }
-    
-    
 }
 
