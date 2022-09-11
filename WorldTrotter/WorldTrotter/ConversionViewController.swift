f2a18ce8941e836aa6e724043c56f5c071a38439
jeongmin4869 <ljm4135@naver.com>
2022-09-11 14:13:34 +0900

    iOS with Swift (숫자 포맷)

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -6,19 +6,62 @@ class ConversionViewControlller : UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    @IBAction func fahrenheitEditingChanged(textField: UITextField){
-        
-        if let text = textField.text, !text.isEmpty{
-            celsiusLabel.text = textField.text
-        }
-        else{
-            celsiusLabel.text = "???"
-        }
-    }
     
     @IBAction func dismissKeyboard(sender: AnyObject){
         textField.resignFirstResponder()
     }
+    
+    // 1
+    @IBAction func fahrenheitEditingChanged(textField: UITextField){
+        
+        if let text = textField.text, let value = Double(text){
+            fahrenheitVlaue = value
+        }
+        else{
+            fahrenheitVlaue = nil
+        }
+    }
+    
+    // 3
+    func updateCelsiusLabel(){
+        if let value = celsiusValue{
+            //celsiusLabel.text = "\(value)"'
+            //celsiusLabel.text = String.init(format: "%.2f", value)
+            celsiusLabel.text = numberFormatter.string(from: NSDecimalNumber(decimal: Decimal(value)))
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    // 4
+    var celsiusValue: Double? {
+        if let value = fahrenheitVlaue{
+            return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    // 2
+    var fahrenheitVlaue: Double?{
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumIntegerDigits = 0
+        nf.maximumIntegerDigits = 1 // 소수점 뒤로 최대 1개까지
+        return nf
+    }()
+    
+    
+    
+    
     
 }
 
