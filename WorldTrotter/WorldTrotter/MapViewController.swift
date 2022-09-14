574da6e9afdcaa38d9895289326515ebb48b3bc8
jeongmin4869 <ljm4135@naver.com>
2022-09-14 22:13:22 +0900

    iOS with Swift (View Controller)

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,13 @@
+//import Foundation
+
+import UIKit
+
+class MapViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view")
+    }
+}
+
