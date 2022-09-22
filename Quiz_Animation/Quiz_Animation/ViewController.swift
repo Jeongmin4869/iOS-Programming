32723dae87ffbcece82204310fa2266ad5332c68
jeongmin4869 <ljm4135@naver.com>
2022-09-22 21:25:56 +0900

    iOS with Swift (Animation)

--- Quiz_Animation/Quiz_Animation/ViewController.swift
+++ Quiz_Animation/Quiz_Animation/ViewController.swift
@@ -11,13 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        nextQuestionLabelCenterXConstraint.constant = -view.frame.width
     }
 
     //@IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     @IBOutlet var currentQuestionLabel: UILabel!
     @IBOutlet var nextQuestionLabel: UILabel!
+    
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
     let questions:[String] = ["From what is cognac made?" , "What is 7 + 7?", "What is the capital of Vermont?"]
     let answers:[String] = ["Grapes", "14", "Montepeller"]
@@ -29,9 +33,9 @@ class ViewController: UIViewController {
             currentQuestionIndex = 0
         }
         let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
-        questionLabel.alpha = 0
+        //questionLabel.alpha = 0
         animationLabelTransition()
         
     }
@@ -42,15 +46,32 @@ class ViewController: UIViewController {
     }
     
     func animationLabelTransition(){
+        /*
         let animationClouse = { () -> Void in
-            self.questionLabel.alpha = 1 // duration동안 최종적으로 도달하는 값
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1 // duration동안 최종적으로 도달하는 값
         }
-        UIView.animate(withDuration: 2.0, animations: animationClouse)
+         */
+        UIView.animate(withDuration: 2.0, delay: 0, options: [],
+                       animations: {
+                        self.currentQuestionLabel.alpha = 0
+                        self.nextQuestionLabel.alpha = 1
+                        self.nextQuestionLabelCenterXConstraint.constant = 0
+                        self.currentQuestionLabelCenterXConstraint.constant = self.view.frame.width
+                        self.view.layoutIfNeeded() // 변경된 Constraint를 적용시킨다.
+                        },
+                       completion: {_ in
+                        swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                        swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                        self.nextQuestionLabelCenterXConstraint.constant = -self.view.frame.width
+                        //self.updateOffScreenLabel()
+                        }
+        )
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        questionLabel.alpha = 0
+        nextQuestionLabel.alpha = 0
     }
 
 }
