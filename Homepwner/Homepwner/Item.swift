edcacd894b3b71f0100715fc6c3211f49bab04dd
jeongmin4869 <ljm4135@naver.com>
2022-09-24 20:22:47 +0900

    iOS with Swift (Item, 블로그 정리)

---
+++ Homepwner/Homepwner/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  Homepwner
+//
+
+import UIKit
+
+class Item : NSObject{
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int){
+        
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = NSDate()
+        
+        super.init()
+    }
+}
---
+++ Homepwner/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by 이정민 on 2022/09/24.
+//
+
+import Foundation
