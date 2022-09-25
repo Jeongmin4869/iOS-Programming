c2bfdb2858e0300e8db3f52dd13de9e9c7119365
jeongmin4869 <ljm4135@naver.com>
2022-09-25 17:44:00 +0900

    iOS with Swift (컨비언스 이니셜라이저)

--- Homepwner/Homepwner/Item.swift
+++ Homepwner/Homepwner/Item.swift
@@ -12,12 +12,36 @@ class Item : NSObject{
     let dateCreated: NSDate
     
     init(name: String, serialNumber: String?, valueInDollars: Int){
+            
+            self.name = name
+            self.valueInDollars = valueInDollars
+            self.serialNumber = serialNumber
+            self.dateCreated = NSDate()
+            
+            super.init()
+    }
+    
+    convenience init(random: Bool = false){
         
-        self.name = name
-        self.valueInDollars = valueInDollars
-        self.serialNumber = serialNumber
-        self.dateCreated = NSDate()
+        if random == false {
+            self.init(name:"", serialNumber: nil, valueInDollars:0)
+            return
+        }
         
-        super.init()
+        let adjectives = ["Fluffy", "Rusty", "Shiny"]
+        let nouns = ["Bear", "Spork","Mac"]
+        
+        var index = Int(arc4random_uniform(UInt32(adjectives.count)))
+        let randdomAdjective = adjectives[index]
+        
+        index = Int(arc4random_uniform(UInt32(nouns.count)))
+        let randdomNoun = nouns[index]
+        
+        let randomName = "\(randdomAdjective)\(randdomNoun)"
+        let randomValue = Int(arc4random_uniform(100))
+        let randomSerial = NSUUID().uuidString.components(separatedBy: "-").first!
+        
+        self.init(name:randomName, serialNumber:randomSerial, valueInDollars:randomValue)
+
     }
 }
