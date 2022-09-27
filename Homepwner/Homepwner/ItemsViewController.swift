030e7aa91d6a797f72230a650129e7f77649cf3b
jeongmin4869 <ljm4135@naver.com>
2022-09-27 22:35:06 +0900

    iOS with Swift (UITableViewCell)

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -14,4 +14,14 @@ class ItemsViewController : UITableViewController{
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
         return itemStore.allItems.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")
+        let item = itemStore.allItems[indexPath.row]
+        cell!.textLabel?.text = item.name
+        cell!.detailTextLabel?.text = String(item.valueInDollars)
+        return cell!
+    }
+    
 }
