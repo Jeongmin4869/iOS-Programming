cb490c23fc8f3829959c65d9e17f3bc1c1fedf3d
jeongmin4869 <ljm4135@naver.com>
2022-09-27 21:22:18 +0900

    iOS with Swift (데이터소스 메소드 구현)

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -7,4 +7,11 @@ import UIKit
 
 class ItemsViewController : UITableViewController{
     var itemStore: ItemStore! // reference
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return itemStore.allItems.count
+    }
 }
