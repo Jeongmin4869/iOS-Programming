93a355b145dadb4c68e0b1f18284474f66fd0cde
jeongmin4869 <ljm4135@naver.com>
2022-10-27 21:53:30 +0900

    iOS with Swift (Archiving)

--- Homepwner/Homepwner/Item.swift
+++ Homepwner/Homepwner/Item.swift
@@ -5,12 +5,30 @@
 
 import UIKit
 
-class Item : NSObject{
+class Item : NSObject, NSCoding{
     var name: String
     var valueInDollars: Int
     var serialNumber: String?
     var dateCreated: NSDate
-    let itemKey: String
+    
+    var itemKey: String //let itemKey: String
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(dateCreated, forKey: "dateCreated")
+        aCoder.encode(itemKey, forKey: "itemKey")
+        aCoder.encode(serialNumber, forKey: "serialNumber")
+        aCoder.encode(valueInDollars, forKey: "valueDollars")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        dateCreated = aDecoder.decodeObject(forKey: "dateCreated") as! NSDate
+        itemKey = aDecoder.decodeObject(forKey: "itemKey") as! String
+        serialNumber = aDecoder.decodeObject(forKey: "serialNumber") as! String?
+        valueInDollars = aDecoder.decodeInteger(forKey: "valueInDollars")
+    }
+    
     
     init(name: String, serialNumber: String?, valueInDollars: Int){
             
