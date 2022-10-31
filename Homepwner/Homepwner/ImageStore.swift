d21b6c7a234df5555e6dab51a9e6f80027990bc6
jeongmin4869 <ljm4135@naver.com>
2022-10-31 21:45:33 +0900

    iOS with Swift (Archiving)

--- Homepwner/Homepwner/ImageStore.swift
+++ Homepwner/Homepwner/ImageStore.swift
@@ -12,13 +12,39 @@ class ImageStore: NSObject{
     
     let cache = NSCache<AnyObject, AnyObject>()
     
+    //itemKey를 파일 이름으로 하는 URL 생성함수
+    func imageURLForKey(key: String) -> URL {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return documentDirectory.appendingPathComponent(key)
+    }
+    
     func setImage(image: UIImage, forKey key: String){
         cache.setObject(image, forKey: key as AnyObject)
+        let imageURL = imageURLForKey(key: key)
+        if let data = image.jpegData(compressionQuality: 0.5){
+            do{
+                try data.write(to: imageURL)
+            }catch{
+                print("Error: writing image to a file")
+            }
+        }
     }
     
     
     func imageForKey(key: String) -> UIImage?{
-        return cache.object(forKey: key as AnyObject) as? UIImage
+        //return cache.object(forKey: key as AnyObject) as? UIImage
+        if let existingImage = cache.object(forKey: key as AnyObject) as? UIImage{
+            return existingImage
+        }
+        
+        let imageURL = imageURLForKey(key: key)
+        guard let imageFromDisk = UIImage(contentsOfFile:  imageURL.path) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key as AnyObject)
+        return imageFromDisk
+         
     }
     
     func deleteImage(key: String){
