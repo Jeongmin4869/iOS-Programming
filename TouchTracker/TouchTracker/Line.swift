2bc117b8ef5ea0f9aad846b1b4e7ad67eafb3af5
jeongmin4869 <ljm4135@naver.com>
2022-11-04 21:02:11 +0900

    iOS with Swift (UITouch)

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by 이정민 on 2022/11/04.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line{
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
