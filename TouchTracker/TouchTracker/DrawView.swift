22094f9658ec3645f7cc6495b1c42485d21bc3ee
jeongmin4869 <ljm4135@naver.com>
2022-11-05 00:36:51 +0900

    iOS with Swift (UITouch Event)

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -4,4 +4,56 @@ class DrawView: UIView{
     
     var currentLine: Line?
     var finishedLines = [Line]()
+    
+    func strokeLine(line: Line){
+        let path = UIBezierPath()
+        path.lineWidth = 10;
+        path.lineCapStyle = CGLineCap.round
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        UIColor.black.setStroke()
+        for line in finishedLines{
+            strokeLine(line: line)
+        }
+        if let line = currentLine{
+            UIColor.red.setStroke()
+            strokeLine(line: line)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        currentLine = Line(begin: location, end: location)
+    
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        currentLine?.end = location
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if var line = currentLine{
+            let touch = touches.first
+            let location = touch?.location(in: self)
+            line.end = location
+            finishedLines.append(line)
+        }
+        
+        currentLine = nil
+        setNeedsDisplay()
+    }
+    
 }
