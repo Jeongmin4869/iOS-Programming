7656c434b4af3089e838303bd7b12b98033948bc
jeongmin4869 <ljm4135@naver.com>
2022-11-17 22:41:59 +0900

    iOS with  Swift (Web Service)

---
+++ Photorama/Photorama/FlickrAPI.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by 이정민 on 2022/11/17.
+//
+
+struct FlickrAPI{
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let APIKey = "a6d819499131071f158fd740860a5a88"
+    
+    private static func flickrURL(method: Method, )
+}
---
+++ Photorama/Photorama/PhotoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by 이정민 on 2022/11/17.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
