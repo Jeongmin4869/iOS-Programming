9752dd18802d041acdf2ed9f104dc3822be760f5
jeongmin4869 <ljm4135@naver.com>
2022-11-28 22:10:42 +0900

    iOS with Swift (디버깅..)

--- Photorama/Photorama/PhotoViewController.swift
+++ Photorama/Photorama/PhotoViewController.swift
@@ -15,10 +15,29 @@ class PhotoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        photoStore.fetchRecentPhotos()
-        print("tlf")
+        photoStore.fetchRecentPhotos(completion: { // 스레드에 의해 실행된다.
+            (photosResult) -> Void in
+            switch photosResult {
+            case let .Success(photos):
+                print("Successfully found \(photos.count) recent photos")
+                if let firstPhoto = photos.first {
+                    self.photoStore.fetchImageForPhoto(photo: firstPhoto){
+                        (imageResult) -> Void in
+                        switch imageResult{
+                        case let .Success(image):
+                            // self.imageView.image = image
+                            OperationQueue.main.addOperation{ // 메인 스레드에의해 실행되는 부분. 
+                                self.imageView.image = image
+                            }
+                        case let .Failure(error):
+                            print("Error downloading image : \(error)")
+                        }
+                    }
+                }
+            case let .Failure(error):
+                print("Error fetching recent photos: \(error)")
+            }
+        })
     }
-
-
 }
 
