ef310202b5c6393ba78b3e71a0116c42e377ed74
jeongmin4869 <ljm4135@naver.com>
2022-12-22 22:49:56 +0900

    iOS with Swift (Auto Layout)

--- Login/Login/LoginViewController.swift
+++ Login/Login/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, DatabaseDelegate  {
+class LoginViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, DatabaseDelegate  {
     
     func onChange(groupDatabaseStr: String) {
         groupDatabase = databaseBroker.loadGroupDatabase()
