202d2c3cbec0028dcc7f09b032236f3761df01c8
jeongmin4869 <ljm4135@naver.com>
2022-12-30 05:57:35 +0900

    iOS with Swift (Setting)

--- Booking/Booking/BookingViewController.swift
+++ Booking/Booking/BookingViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class BookingViewController: UIViewController {
 
+    var userName: String! = "gdhong"
+    var userGroup: String! = "TennisCourt"
+    
+    let stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
