66e231833a028da26893f8fb07c47739931322ef
jeongmin4869 <ljm4135@naver.com>
2023-01-17 23:42:58 +0900

    iOS with Swift (TapGesture)

---
+++ Booking/Booking/BookingTapGestureRecognizer.swift
@@ -0,0 +1,13 @@
+//
+//  BookingTapGestureRecognizer.swift
+//  Booking
+//
+//  Created by 이정민 on 2023/01/17.
+//
+
+import Foundation
+import UIKit
+
+class BookingTapGestureRecognizer: UITapGestureRecognizer {
+    var index: Int?
+}
