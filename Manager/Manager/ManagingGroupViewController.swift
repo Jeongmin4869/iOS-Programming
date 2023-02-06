2748279411a59410b10635b6ddeb08b7dea3d391
jeongmin4869 <ljm4135@naver.com>
2023-02-06 21:57:53 +0900

    iOS with Swift

--- Manager/Manager/ManagingGroupViewController.swift
+++ Manager/Manager/ManagingGroupViewController.swift
@@ -1,13 +1,38 @@
 import UIKit
 
-class ManagingGroupViewController: UITableViewController {
+class ManagingGroupViewController: UITableViewController, DatabaseDelegate{
     
+    var databaseBroker : DatabaseBroker!
+    var groupDatabase: [String]!
     
     override func viewDidLoad() {
-        print("..ManagingGroupViewController..")
-        self.navigationItem.title = "Group"
         
+        databaseBroker = DatabaseObject.createDatabase(rootPath: "test")
+        databaseBroker.setGroupDataDelegate(dataDelegate: self)
+        
+        groupDatabase = databaseBroker.loadGroupDatabase()
         
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupDatabase.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "GroupCell")
+        let group = groupDatabase[indexPath.row]
+        cell.textLabel?.text = group
+        return cell
+    }
+    
+    
+    func onChange(groupDatabaseStr: String) {
+        groupDatabase = databaseBroker.loadGroupDatabase()
+    }
+    
     
 }
