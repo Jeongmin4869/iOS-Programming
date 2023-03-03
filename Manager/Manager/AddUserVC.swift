a6fcc40eda41f02bae690d577107ea43e3b002d0
jeongmin4869 <ljm4135@naver.com>
2023-03-03 22:52:27 +0900

    iOS with Swift

--- Manager/Manager/AddUserVC.swift
+++ Manager/Manager/AddUserVC.swift
@@ -1,9 +1,11 @@
 import UIKit
 
+protocol saveUserDelegate {
+    func sendData(data: String)
+}
+
 
 class AddUserVC: UIViewController {
-    
-    
     
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var pwLabel: UILabel!
@@ -15,23 +17,35 @@ class AddUserVC: UIViewController {
     
     @IBOutlet weak var groupPicker: UIPickerView!
     
-    @IBOutlet weak var calcelBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
     @IBOutlet weak var addBtn: UIButton!
+    
+    var delegate: saveUserDelegate?
     
     override func viewDidLoad() {
         mainLabel.text = "User"
         idLabel.text = "ID"
-        pwLabel.text = "Password"
+        pwLabel.text = "PW"
+        idLabel.textAlignment = .right
+        pwLabel.textAlignment = .right
+        mainLabel.font = UIFont.systemFont(ofSize: 30)
+        mainLabel.frame.size.width = 200
+        mainLabel.textAlignment = .center
+        idLabel.font = UIFont.systemFont(ofSize: 15)
+        pwLabel.font = UIFont.systemFont(ofSize: 15)
         
-        calcelBtn.setTitle("Calcel", for: .normal)
+        cancelBtn.setTitle("Calcel", for: .normal)
+        cancelBtn.frame.size.width = 100
         addBtn.setTitle("Add", for: .normal)
     }
     
     
     @IBAction func clickCalcelBtn(_ sender: Any) {
+        self.dismiss(animated: true)
     }
     
     @IBAction func clickAddBtn(_ sender: Any) {
+        
     }
     
     
